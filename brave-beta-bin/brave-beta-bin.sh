86dca19c8f37be76bd42830ce9a52e278ec9b683
Narnia <105121324+narnicodes@users.noreply.github.com>
2022-05-08 18:18:07 +0530

    Delete brave-beta-bin directory

--- brave-beta-bin/brave-beta-bin.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z "${CHROME_USER_DATA_DIR}" ]]; then
-    export CHROME_USER_DATA_DIR=~/.config/BraveSoftware/Brave-Browser-Beta
-fi
-exec "/opt/brave.com/brave-beta/brave-browser" "$BRAVE_FLAGS" "$FLAG" "$@"
